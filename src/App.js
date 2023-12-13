8e692a2630ca82f1b3a7999b915dfea14e8a9d22
Vicente Garcia <vicentegarciaayllon@gmail.com>
2023-12-13 20:48:07 +0100

    finish intro and started primer beso

--- src/App.js
+++ src/App.js
@@ -9,22 +9,38 @@ function App() {
       <header className="App-header">🪐⚡️</header>
       <body className="App-body">
         <div className="main-content">
-          <div className="intro">
-            <p>
-              En una época de hacer balance, me apetecía echar la vista atrás para
-              revivir lo que somos y lo que hemos contruído.
-            </p>
-          </div>
+          {step === 0 && (
+            <div className="intro">
+              <div></div>
+              <p>
+                En una época de hacer balance, me apetecía echar la vista atrás
+                para reconocer lo que somos y lo que hemos construído.
+              </p>
+            </div>
+          )}
+          {step === 1 && (
+            <div className="primer-beso">
+              <p>
+                Te lo he dicho muchas veces, pero esta ha sido la mejor noche de mi vida.
+              </p>
+              <div></div>
+            </div>
+          )}
         </div>
         <div className="controllers">
           {step !== 0 ? (
             <button className="previous-btn" onClick={() => setStep(step - 1)}>
-            ➔
+              {"﹤"}
             </button>
           ) : undefined}
-          <h2>Bienvenida</h2>
+          {step === 0 && (
+            <h2>Empecemos</h2>
+          )}
+          {step === 1 && (
+            <h2>Primer Beso</h2>
+          )}
           <button className="next-btn" onClick={() => setStep(step + 1)}>
-            ➔
+            ﹥
           </button>
         </div>
       </body>
